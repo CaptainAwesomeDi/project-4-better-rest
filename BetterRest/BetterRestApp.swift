e0cd3ca40bf381a2fc2dba2b63f20529a37a886b
Di Wu <di.wu@productboard.com>
2021-12-05 18:56:16 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Di Wu on 2021-12-05.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
